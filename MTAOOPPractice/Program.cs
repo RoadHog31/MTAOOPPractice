895a54e700aa801e457334d7c350a9b63c48b80a
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-11 10:20:17 +0000

    Tidied up code

--- MTAOOPPractice/Program.cs
+++ MTAOOPPractice/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MTAOOPPractice
 {
@@ -10,17 +6,11 @@ namespace MTAOOPPractice
     {
         static void Main(string[] args)
         {
-            //Rectangle rect = new Rectangle(10.0, 20.0);
-            //double area = rect.GetArea();
-            //Console.WriteLine("Area of the Rectangle is: {0}", area);
-
             Rectangle rect = new Rectangle();
             rect.Length = 10.0;
             rect.Width = 20.0;
             double area = rect.GetArea();
             Console.WriteLine("Area of Rectangle = {0}", area);
-
-
         }
     }
 }
